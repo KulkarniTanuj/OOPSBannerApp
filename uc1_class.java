3c74e89ab13ae9690b561768ccb174c7da346280
kullkarnitanuj <designommetallics@gmail.com>
2026-03-03 10:59:47 +0530

    uc1

---
+++ uc1_class.java
@@ -0,0 +1,13 @@
+public class uc1_class {
+
+    /**
+     * Main method – Entry point of the Java application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Printing the literal text OOPS
+        System.out.println("OOPS");
+
+    }
+}
